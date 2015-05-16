bc64da86a0a997dc22ed0a1f451e06ed6b14ed53
Administrator <elias.muller1@gmail.com>
2015-05-15 23:44:55 -0300

    model whit name test

---
+++ models/TestNameModel.php
@@ -0,0 +1,11 @@
+<?php
+use core\Model as Model;
+
+class TestNameModel extends Model{
+    private $table = 'teste';
+    public function insertTest(){
+
+        $this->insert($this->table,['nome'=>'Elias']);
+
+    }
+}
