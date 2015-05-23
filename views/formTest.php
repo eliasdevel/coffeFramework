f21c16d2e7f1b667853238b29ab9e56edb27e72e
Administrator <elias.muller1@gmail.com>
2015-05-23 00:55:35 -0300

    jquery + bootstrap3

--- views/formTest.php
+++ views/formTest.php
@@ -1,13 +1,27 @@
 <?php
 
-use core\library\Path as Path;
-?>
-<form action="<?= Path::baseUrl()?>/elias/save" method="post">
+use core\library\Path as Path; ?>
+<form class="form-horizontal">
+    <fieldset>
+        <!-- Form Name -->
+        <center><legend>Login</legend></center>
 
-    <label for="name" >Name:</label>
-    <input type="text" name="name" />
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="textinput">User:</label>
+            <div class="col-md-4">
+                <input id="textinput" name="textinput" type="text" placeholder="placeholder"
+                       class="form-control input-md">
+            </div>
+        </div>
 
-    <label for="name" >Email:</label>
-    <input type="email" name="email" />
-
+        <!-- Password input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="passwordinput">Password:</label>
+            <div class="col-md-4">
+                <input id="passwordinput" name="passwordinput" type="password" placeholder="placeholder"
+                       class="form-control input-md">
+            </div>
+        </div>
+    </fieldset>
 </form>
--- views/layout.php
+++ views/layout.php
@@ -1,14 +1,18 @@
 <?php
-
 use core\library\Path as Path;
 ?>
 <!DOCTYPE html>
-<html>
-<head lang="en">
-    <meta charset="UTF-8">
-    <title>Page not Found</title>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= isset($title) ? $title : 'Title'  ?></title>
+    <link href="<?= Path::baseUrl()?>/vendor/twitter/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body >
+<body>
+<script src="<?= Path::baseUrl()?>/vendor/components/jquery/jquery.js"></script>
+<script src="<?= Path::baseUrl()?>/vendor/twitter/bootstrap/dist/js/bootstrap.min.js"></script>
 <?php view($contentView) ?>
 </body>
 </html>
